69ee35a44eb829f10fcf0103a24a86cebd4460a4
tspaja2025 <tspaja2025@outlook.com>
2026-05-20 10:10:56 +0300

    Update main.go
    Volume conversions

--- main.go
+++ main.go
@@ -185,51 +185,51 @@ func convertVolume(value float64, from, to string) float64 {
 func toLiters(unit string) float64 {
 	switch unit {
 	case "Cubic Meter":
-		return 1.0
+		return 1000.0
 	case "Cubic Kilometer":
-		return 1.0
+		return 1e12
 	case "Cubic Centimeter":
-		return 1.0
+		return 0.001
 	case "Cubic Millimeter":
-		return 1.0
+		return 0.000001
 	case "Liter":
 		return 1.0
 	case "Milliliter":
-		return 1.0
+		return 0.001
 	case "US Gallon":
-		return 1.0
+		return 3.78541
 	case "US Quart":
-		return 1.0
+		return 0.946353
 	case "US Pint":
-		return 1.0
+		return 0.473176
 	case "US Cup":
-		return 1.0
+		return 0.24
 	case "US Fluid Ounce":
-		return 1.0
+		return 0.0295735
 	case "US Table Spoon":
-		return 1.0
+		return 0.0147868
 	case "US Tea Spoon":
-		return 1.0
+		return 0.00492892
 	case "Imperial Gallon":
-		return 1.0
+		return 4.54609
 	case "Imperial Quart":
-		return 1.0
+		return 1.13652
 	case "Imperial Pint":
-		return 1.0
+		return 0.568261
 	case "Imperial Fluid Ounce":
-		return 1.0
+		return 0.0284131
 	case "Imperial Table Spoon":
-		return 1.0
+		return 0.0177582
 	case "Imperial Tea Spoon":
-		return 1.0
+		return 0.00591939
 	case "Cubic Mile":
-		return 1.0
+		return 4.16818e12
 	case "Cubic Yard":
-		return 1.0
+		return 764.555
 	case "Cubic Foot":
-		return 1.0
+		return 28.3168
 	case "Cubic Inch":
-		return 1.0
+		return 0.0163871
 	default:
 		return 1.0
 	}
